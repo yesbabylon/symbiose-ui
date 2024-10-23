a5ced9f12c1f763af4e2610193ca1d44ddd5efc2
Cédric Françoys <cedricfrancoys@gmail.com>
2024-10-23 15:28:14 +0200

    syntax

--- sb-shared-lib/projects/sb-shared-lib/src/lib/services/http.interceptor.service.ts
+++ sb-shared-lib/projects/sb-shared-lib/src/lib/services/http.interceptor.service.ts
@@ -4,39 +4,39 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/c
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
     private storage: any;
 
-    constructor() { 
+    constructor() {
         this.storage = localStorage;
     }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.handleAccess(request, next));
-  }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return from(this.handleAccess(request, next));
+    }
 
-  private getAccessToken() {
-    return this.storage.getItem('access_token');
-  }
+    private getAccessToken() {
+        return this.storage.getItem('access_token');
+    }
 
-  private handleAccess(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-      // const token = this.getAccessToken();
+    private handleAccess(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+        // const token = this.getAccessToken();
 
-      req = req.clone({
+        req = req.clone({
         // required when using httpOnly cookie for Auth
         withCredentials: true
-/*
+    /*
         // For Mobile: we're using HTTP from webview, origin is set to http://localhost (android) or capacitor://localhost (iOS)
         // add a custom X-App-ID header to emit CORS response allowing any request having this header set to an authorized value
         setHeaders: {
-          Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${token}`
         }
-*/        
-      });
-  
-      return next.handle(req).toPromise();
-  }
+    */
+        });
+
+        return next.handle(req).toPromise();
+    }
 }
