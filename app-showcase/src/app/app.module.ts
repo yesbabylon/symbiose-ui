11e3193cf13f146dbe6ecf83b8ba0a85c8e0566f
Cédric Françoys <cedricfrancoys@gmail.com>
2025-09-03 08:58:16 +0200

    fixed material components dependencies

--- app-showcase/src/app/app.module.ts
+++ app-showcase/src/app/app.module.ts
@@ -6,29 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { Platform, PlatformModule } from '@angular/cdk/platform';
 
-// @ts-ignore
 import { SharedLibModule, AuthInterceptorService, CustomDateAdapter } from 'sb-shared-lib';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppRootComponent } from './app.root.component';
+import { AppComponent } from './in/app.component';
 
-import { MatTableModule } from '@angular/material/table';
 /* HTTP requests interception dependencies */
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
-// specific locale setting
-import localeFr from '@angular/common/locales/fr';
-import { AppComponent } from './in/app.component';
 import { ComponentPresenterComponent } from './_components/component-presenter/component-presenter.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { DynamicHostDirective } from './_directives/dynamic-host.directive';
 import { DocumentationOverlayComponent } from './_components/documentation-overlay/documentation-overlay.component';
-import { MatTabsModule } from '@angular/material/tabs';
 
 registerLocaleData(localeFr);
 
@@ -47,11 +41,7 @@ registerLocaleData(localeFr);
 		SharedLibModule,
 		MatNativeDateModule,
 		PlatformModule,
-		NgxMaterialTimepickerModule.setLocale('fr-BE'),
-		MatTableModule,
-		MatButtonModule,
-		MatIconModule,
-		MatTabsModule,
+		NgxMaterialTimepickerModule.setOpts('fr-BE', 'latn')
 	],
 	providers: [
 		// add HTTP interceptor to inject AUTH header to any outgoing request
